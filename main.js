c73bc25a9c3cd3e3b924c2f80fe038e0dc4a8ac4
ROHIT KUMAR <rk15072000@gmail.com>
2023-01-27 10:22:31 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,99 @@
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
+let uppass = [];
+let inpass = [];
+
+
+// adding and removing border
+function upimg(element) {
+    var Image = element.querySelector('img');
+    if (Image) {
+        if (Image.classList.contains('clicked')) {
+            Image.classList.remove('clicked');
+            uppass.splice(uppass.indexOf(element.id), 1);
+            // console.log(element.id);
+            // console.log(uppass);
+        }
+        else {
+            Image.classList.add('clicked');
+            uppass.push(element.id);
+            // console.log(element.id);
+            // console.log(uppass);
+        }
+    }
+}
+
+function inimg(element) {
+    var Image = element.querySelector('img');
+    if (Image) {
+        if (Image.classList.contains('clicked')) {
+            Image.classList.remove('clicked');
+            inpass.splice(inpass.indexOf(element.id), 1);
+            // console.log(element.id);
+            // console.log(inpass);
+        }
+        else {
+            Image.classList.add('clicked');
+            inpass.push(element.id);
+            // console.log(element.id);
+            // console.log(inpass);
+        }
+    }
+}
+// element image recognition
+function signup() {
+    // e.preventDefault()
+    console.log(document.getElementById('upmail').value +",,,,"+uppass.length)
+    if(document.getElementById('upmail').value===""||uppass.length===0){
+        var myText= "Please fill all the fields";
+        alert(myText);
+        
+    }else{
+    sessionStorage.setItem("upname", document.getElementById('upmail').value);
+    sessionStorage.setItem("uppass", uppass);
+    var myText = "Account Created Succesfully";
+    alert(myText);
+    }
+    // console.log(document.getElementById('upmail').value+ uppass.length)
+}
+// image pattern authentication
+var v2 = new Boolean(false);
+function signin() {
+    let str = document.getElementById('inmail').value;
+    let array = sessionStorage.getItem("uppass");
+    let check1 = array.localeCompare(inpass.toString());
+    if ((!str.localeCompare(sessionStorage.getItem("upname"))) && !check1) {
+        var myText = "Login is successful";
+        alert(myText);
+        NewTab();
+        
+    }
+    else{
+        var myText = "Login Failed";
+        alert(myText);
+    
+        sendMail3();
+    
+    }
+}
+
+function sendMail3(){
+    emailjs.send('service_7q1sn6s', 'template_v7f98gs')
+    .then(function(res){
+        // console.log("Success", res.status);
+        alert("mail sent successfully");
+    })
+}
+function sendMail2(){
+    emailjs.send('service_7q1sn6s', 'template_ogw30ms')
+    .then(function(res){
+        // console.log("Success", res.status);
+        alert("mail sent successfully");
+    })
+}
+
+function NewTab() {
+    window.open(
+      "https://sih.gov.in/", "_blank");
+}
